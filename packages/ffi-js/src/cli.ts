26b34069e487359f8068dbd88b33dd3cbead8bfd
江夏尧 <3446798488@qq.com>
2024-12-24 01:29:38 +0000

    fix: 修复 cli 入口

--- packages/ffi-js/src/cli.ts
+++ packages/ffi-js/src/cli.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { Command } from 'commander';
 import { getCliParams } from './gen/proto.js';
 import { getAllVersions, getVersionBinary, matchPlatform } from './load.js';
